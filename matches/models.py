0e4b9fec8729922cb7ec996436a70c58ac87b195
이정훈 <glen@ijeonghun-ui-MacBookAir.local>
2023-05-28 21:09:03 +0900

    set first match model

--- matches/models.py
+++ matches/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Match(models.Model) :
+
+    """ Model Definition for Match """
+
+    type = models.CharField(max_length=140)
+    email = models.EmailField()
+    # host_id = 
+    host_point = models.PositiveIntegerField()
+    # guest_id =
+    guest_point = models.PositiveIntegerField()
+    # winner_id = 
+    # loser_id = 
+    start_time : models.DateTimeField()
+    end_time : models.DateTimeField()
