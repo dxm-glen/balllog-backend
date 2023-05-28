b42d81080c33d8acc36f63211ac8df23ce557dce
이정훈 <glen@ijeonghun-ui-MacBookAir.local>
2023-05-28 23:02:28 +0900

    add TYPE_CHOICE

--- matches/admin.py
+++ matches/admin.py
@@ -3,5 +3,11 @@ from .models import Match
 
 @admin.register(Match) # match 모델에 대해서 다룰 거라는 데코레이터
 class MatchAdmin(admin.ModelAdmin):
-    pass
-
+    
+    list_display = (
+        "type", "host_point", "guest_point", "start_time", "end_time",
+    )
+    list_filter = (
+        "type", "end_time",
+    )
+    search_fields = ("type",)
--- matches/models.py
+++ matches/models.py
@@ -3,16 +3,20 @@ from django.db import models
 class Match(models.Model) :
 
     """ Model Definition for Match """
+    TYPE_CHOICES = [
+        ("NORMAL", "normal"),
+        ("OFFICIAL", "official"),
+    ]
 
-    type = models.CharField(max_length=140, default="normal") #defalut = normal
+    type = models.CharField(max_length=140, choices=TYPE_CHOICES, default="NORMAL") #defalut = normal
     # host_id = 
     host_point = models.PositiveIntegerField()
     # guest_id =
     guest_point = models.PositiveIntegerField()
     # winner_id = 
     # loser_id = 
-    start_time : models.DateTimeField()
-    end_time : models.DateTimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
 
     def __str__(self):
-        return self.name
+        return self.type
