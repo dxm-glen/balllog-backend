6409eb200ff8d908fd847d3c5fe1fc1057ca9839
이정훈 <glen@ijeonghun-ui-MacBookAir.local>
2023-05-28 20:54:09 +0900

    create matches app

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr' #en-us
 
 TIME_ZONE = 'Asia/Seoul'
 
